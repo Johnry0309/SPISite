088b60a77bcb357f6b4e10daa112612356f3a6a4
Johnry0309 <johnryqadornado0309@gmail.com>
2025-04-29 21:26:11 +0800

    enrollment tab added

--- myapp/templatetags/custom_filters.py
+++ myapp/templatetags/custom_filters.py
@@ -2,6 +2,21 @@ from django import template
 
 register = template.Library()
 
+
+
+@register.filter
+def get_attr(obj, attr_name):
+    return getattr(obj, attr_name, False)  # add fallback default
+
 @register.filter
 def split(value, arg):
     return value.split(arg)[-1]
+
+@register.filter
+def replace(value, arg):
+    old, new = arg.split(',')
+    return value.replace(old, new)
+
+@register.filter
+def replace_underscores(value):
+    return value.replace('_', ' ').title()
